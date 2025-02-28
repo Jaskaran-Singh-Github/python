b013d6e1f69e059470f6cc1209cb6463e8a088fb
Jaskaran-Singh-Github <sjaskarn387@gmail.com>
2025-02-28 22:05:22 +0530

    Add files via upload

---
+++ calculatorExercise-1.py
@@ -0,0 +1,16 @@
+# print(12+3)#Add
+# print(12-3)#Sub
+# print(12*3)#Multiply
+# print(12/3)#Division
+# print(25%3)#Modulus
+# print(25//3)#Floor Division
+# print(12**3)#Power
+# a=int(input("Enter a First Number:"))
+# b=int(input("Enter a Second Number:"))
+# print(a+b)
+# print(a-b)
+# print(a*b)
+# print(a/b)
+# print(a%b)
+# print(a//b)
+# print(a**b)
---
+++ kaun banega crorepati Exercise-3.py
@@ -0,0 +1,12 @@
+print(input("Your Good name:"))
+print("Welcome To kaun Banega Crorepati")
+print("Your question is and your correct answer will be you are a Crorepati and win the game")
+question=("Where is Red fort Located?")
+options=("A.Agra,B.Kolkata,C.Delhi,D.Mumbai")
+print(question)
+print(options)
+answer=input("Enter your correct option:")
+if(answer=='C'):
+    print("1 crore")
+else:
+    print("manavar app ne galat jawab diya")
